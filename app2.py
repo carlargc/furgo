53df4bc82ec98de45ef2851bcbca94b59b7090af
Carla <115326329+carlargc@users.noreply.github.com>
2024-11-12 01:36:51 -0300

    Update app2.py
    comente whit app

--- app2.py
+++ app2.py
@@ -1515,8 +1515,8 @@ if __name__ == "__main__":
 
 
 
-with app.app_context():
-    db.create_all()
+#with app.app_context():
+  #  db.create_all()
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
